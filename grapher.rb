5dede3299b6d9c2c4e5f58e4de225b4f42590e64
David E Worth <dave@statype.com>
2022-09-04 19:58:51 +0000

    add line graph
    This scaffolds the assignment

--- grapher.rb
+++ grapher.rb
@@ -1,6 +1,11 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "apexcharts"
+
 get "/" do
+  @daily_activity = Array.new(40) { rand(1...9) }
+  @increasing = 1.upto(40).map { |v| v + rand(1...5) }
+
   erb :index
 end
